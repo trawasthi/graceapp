f118a4112ed486c0306a7aa0d90b42a8632d63f7
Abhinav Gajurel <abhinav@Abhinavs-MacBook-Pro.local>
2019-09-20 00:49:33 +1000

    home layout changes

--- src/homescreen.js
+++ src/homescreen.js
@@ -9,113 +9,157 @@ import {
   Card,
   CardItem,
   Thumbnail,
-  Icon
+  Icon,
+  Right
 } from 'native-base';
+import { StyleSheet } from 'react-native';
 import educationLogo from '../assets/education.png';
 import migrationLogo from '../assets/migration.jpg';
 import studyLogo from '../assets/study.png';
 import professionalLogo from '../assets/professional.png';
 import visaLogo from '../assets/visa.png';
+import otherLogo from '../assets/other.png';
 import { withFirebase } from './firebase/firebase';
 
 class HomeScreen extends React.Component {
   render() {
     return (
-      <Container>
-        <Content padder>
-          <Card>
-            <CardItem button onPress={() => this.props.navigation.navigate('Education')}>
-              <Left>
-                <Thumbnail square source={educationLogo} />
-                <Body>
-                  <Text>Education</Text>
-                </Body>
-              </Left>
-            </CardItem>
-          </Card>
+      <Container style={[style.parent]}>
+        <Card transparent style={[style.child]}>
+          <CardItem
+            button
+            onPress={() => this.props.navigation.navigate('Education')}
+            style={{ alignItems: 'center', flex: 1 }}>
+            <Thumbnail square source={educationLogo} style={{ width: 135, height: 125 }} />
+          </CardItem>
+          <CardItem
+            button
+            onPress={() => this.props.navigation.navigate('Education')}
+            footer
+            style={{ flexDirection: 'column', justifyContent: 'center' }}>
+            <Text>Education</Text>
+          </CardItem>
+        </Card>
 
-          <Card>
-            <CardItem button onPress={() => this.props.navigation.navigate('Migration')}>
-              <Left>
-                <Thumbnail square source={migrationLogo} />
-                <Body>
-                  <Text>Migration</Text>
-                </Body>
-              </Left>
-            </CardItem>
-          </Card>
+        <Card transparent style={[style.child]}>
+          <CardItem
+            button
+            onPress={() => this.props.navigation.navigate('Migration')}
+            style={{ alignItems: 'center', flex: 1 }}>
+            <Thumbnail square source={migrationLogo} style={{ width: 135, height: 125 }} />
+          </CardItem>
+          <CardItem
+            button
+            onPress={() => this.props.navigation.navigate('Migration')}
+            footer
+            style={{ flexDirection: 'column', justifyContent: 'center' }}>
+            <Text>Migration</Text>
+          </CardItem>
+        </Card>
 
-          <Card>
-            <CardItem>
-              <Left>
-                <Thumbnail square source={studyLogo} />
-                <Body>
-                  <Text>PTE/IELTS</Text>
-                </Body>
-              </Left>
-            </CardItem>
-          </Card>
+        <Card transparent style={[style.child]}>
+          <CardItem
+            button
+            onPress={() => this.props.navigation.navigate('Migration')}
+            style={{ alignItems: 'center', flex: 1 }}>
+            <Thumbnail square source={studyLogo} style={{ width: 135, height: 125 }} />
+          </CardItem>
+          <CardItem
+            button
+            onPress={() => this.props.navigation.navigate('Migration')}
+            footer
+            style={{ flexDirection: 'column', justifyContent: 'center' }}>
+            <Text>PTE/IELTS</Text>
+          </CardItem>
+        </Card>
 
-          <Card>
-            <CardItem>
-              <Left>
-                <Thumbnail square source={professionalLogo} />
-                <Body>
-                  <Text>Professional Year</Text>
-                </Body>
-              </Left>
-            </CardItem>
-          </Card>
+        <Card transparent style={[style.child]}>
+          <CardItem
+            button
+            onPress={() => this.props.navigation.navigate('Migration')}
+            style={{ alignItems: 'center', flex: 1 }}>
+            <Thumbnail square source={professionalLogo} style={{ width: 135, height: 125 }} />
+          </CardItem>
+          <CardItem
+            button
+            onPress={() => this.props.navigation.navigate('Migration')}
+            footer
+            style={{ flexDirection: 'column', justifyContent: 'center' }}>
+            <Text>Professional Year</Text>
+          </CardItem>
+        </Card>
 
-          <Card>
-            <CardItem>
-              <Left>
-                <Thumbnail square source={visaLogo} />
-                <Body>
-                  <Text>Visa Options</Text>
-                </Body>
-              </Left>
-            </CardItem>
-          </Card>
+        <Card transparent style={[style.child]}>
+          <CardItem
+            button
+            onPress={() => this.props.navigation.navigate('Migration')}
+            style={{ alignItems: 'center', flex: 1 }}>
+            <Thumbnail square source={visaLogo} style={{ width: 135, height: 125 }} />
+          </CardItem>
+          <CardItem
+            button
+            onPress={() => this.props.navigation.navigate('Migration')}
+            footer
+            style={{ flexDirection: 'column', justifyContent: 'center' }}>
+            <Text>Visa Options</Text>
+          </CardItem>
+        </Card>
 
-          {/* <Card>
-            <CardItem>
-              <Left>
-                <Thumbnail square source={serviceLogo} />
-                <Body>
-                  <Text>Other Services</Text>
-                </Body>
-              </Left>
-            </CardItem>
-          </Card> */}
-
-          <Button
-            style={{
-              textAlign: 'center',
-              justifyContent: 'center',
-              alignItems: 'center',
-              marginTop: 8,
-              backgroundColor: '#2196f3'
-            }}
-            primary
-            rounded
-            onPress={() => {
-              this.props.navigation.navigate('Booking');
-            }}>
-            <Text style={{ fontWeight: 'bold' }}> Book Appointment </Text>
-          </Button>
-        </Content>
+        <Card transparent style={[style.child]}>
+          <CardItem
+            button
+            onPress={() => this.props.navigation.navigate('Migration')}
+            style={{ alignItems: 'center', flex: 1 }}>
+            <Thumbnail square source={otherLogo} style={{ width: 135, height: 125 }} />
+          </CardItem>
+          <CardItem
+            button
+            onPress={() => this.props.navigation.navigate('Migration')}
+            footer
+            style={{ flexDirection: 'column', justifyContent: 'center' }}>
+            <Text>Other Services</Text>
+          </CardItem>
+        </Card>
+        <Button
+          style={{
+            textAlign: 'center',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: '#2196f3',
+            marginLeft: 75
+          }}
+          primary
+          rounded
+          onPress={() => {
+            this.props.navigation.navigate('Booking');
+          }}>
+          <Text style={{ fontWeight: 'bold' }}> Book Appointment </Text>
+        </Button>
       </Container>
     );
   }
 }
+
+const style = StyleSheet.create({
+  parent: {
+    width: '100%',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginLeft: 20
+  },
+  child: {
+    width: '44%',
+    margin: '1%',
+    aspectRatio: 1
+  }
+});
 
 const Home = withFirebase(HomeScreen);
 Home.navigationOptions = ({ navigation }) => {
   return {
     title: 'Grace Consultancy',
     headerStyle: {
-      backgroundColor: '#f44336'
+      backgroundColor: '#2196f3'
     },
     headerTintColor: '#fff',
     headerTitleStyle: {
