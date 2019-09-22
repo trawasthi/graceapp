c0f95b5b49e47ce74713f20b461168422b259520
Abhinav Gajurel <abhinav@abhinavs-mbp.home>
2019-09-22 16:19:41 +1000

    cloud function fixes

--- email_notifier/functions/index.js
+++ email_notifier/functions/index.js
@@ -23,20 +23,27 @@ exports.sendConfirmationEmail = functions.https.onRequest((req, res) => {
   const email = req.query.email;
   const displayName = req.query.name;
   const time = req.query.time;
-  return sendWelcomeEmail(email, displayName, time);
+  sendWelcomeEmail(email, displayName, time)
+    .then(message => {
+      res.send({ status: message });
+      return;
+    })
+    .catch(error => {
+      res.send({ status: 'Error', error });
+    });
 });
 
 // Sends a welcome email to the given user.
 async function sendWelcomeEmail(email, displayName, time) {
   const mailOptions = {
-    from: `${APP_NAME} <noreply@firebase.com>`,
+    from: `${APP_NAME} <avgldnl@gmail.com>`,
     to: email
   };
 
   // The user subscribed to the newsletter.
   mailOptions.subject = `Appointment with ${APP_NAME}!`;
   mailOptions.text = `Hey ${displayName ||
-    ''}! Welcome to ${APP_NAME}. Your appointment is confirmed and is scheduled to ${time}.`;
+    ''}! \nWelcome to ${APP_NAME}. Your appointment is confirmed and is scheduled to ${time}.\nWe look forward to seeing you.\n\nHave a great day ahead.\nGrace Consultancy`;
   await mailTransport.sendMail(mailOptions);
-  return null;
+  return 'Email sent';
 }
