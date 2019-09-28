1df96b18992b5177c0233d71d95670b095c26007
Abhinav Gajurel <abhinav@Abhinavs-MacBook-Pro.local>
2019-09-28 18:57:23 +1000

    add link functions

--- src/links.js
+++ src/links.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
-import { Container, Content, Card, CardItem, Body, Icon } from 'native-base';
+import {
+  Content,
+  Body,
+  Text,
+  Icon,
+  Right,
+  Left,
+  View,
+  Header,
+  Container,
+  ListItem,
+  List
+} from 'native-base';
+import { Linking } from 'react-native';
 
-export default class Links extends Component {
+export default class Gallery extends Component {
   static navigationOptions = {
     drawerLabel: 'Useful links',
     drawerIcon: () => <Icon name="link" />
@@ -9,24 +22,97 @@ export default class Links extends Component {
 
   render() {
     return (
-      <Container>
-        <Content padder>
-          <Card>
-            <CardItem>
-              <Body>
-                <Icon
-                  name="menu"
-                  size={30}
-                  style={{ marginStart: 10 }}
-                  backgroundColor="#000000"
-                  onPress={() => this.props.navigation.openDrawer()}>
-                  Links
-                </Icon>
-              </Body>
-            </CardItem>
-          </Card>
-        </Content>
-      </Container>
+      <View style={{ flex: 1 }}>
+        <Header style={{ backgroundColor: '#009688' }}>
+          <Left>
+            <Icon
+              name="menu"
+              size={30}
+              style={{ marginStart: 10 }}
+              onPress={() => this.props.navigation.openDrawer()}
+            />
+          </Left>
+          <Body>
+            <Text style={{ fontWeight: 'bold' }}>Links</Text>
+          </Body>
+          <Right />
+        </Header>
+        <Container>
+          <Content padder>
+            <List>
+              <ListItem
+                onPress={() => Linking.openURL('https://afpnationalpolicechecks.converga.com.au/')}>
+                <Body>
+                  <Text>National Police Check (AU)</Text>
+                </Body>
+                <Right>
+                  <Icon name="arrow-forward" />
+                </Right>
+              </ListItem>
+              <ListItem
+                onPress={() => Linking.openURL('https://www.vfsglobal.com/india/australia/')}>
+                <Body>
+                  <Text>Police Clearance India</Text>
+                </Body>
+                <Right>
+                  <Icon name="arrow-forward" />
+                </Right>
+              </ListItem>
+              <ListItem onPress={() => Linking.openURL('http://www.bupamvs.com.au/')}>
+                <Body>
+                  <Text>Medical Examination (AU)</Text>
+                </Body>
+                <Right>
+                  <Icon name="arrow-forward" />
+                </Right>
+              </ListItem>
+              <ListItem
+                onPress={() => Linking.openURL('https://www.vfsglobal.com/india/australia/')}>
+                <Body>
+                  <Text>Indian Passport</Text>
+                </Body>
+                <Right>
+                  <Icon name="arrow-forward" />
+                </Right>
+              </ListItem>
+              <ListItem
+                onPress={() =>
+                  Linking.openURL(
+                    'https://www.australia.gov.au/about-australia/special-dates-and-events/public-holidays'
+                  )
+                }>
+                <Body>
+                  <Text>Public Holidays (AU)</Text>
+                </Body>
+                <Right>
+                  <Icon name="arrow-forward" />
+                </Right>
+              </ListItem>
+              <ListItem onPress={() => Linking.openURL('https://www.realestate.com.au/buy')}>
+                <Body>
+                  <Text>Real Estate</Text>
+                </Body>
+                <Right>
+                  <Icon name="arrow-forward" />
+                </Right>
+              </ListItem>
+              <ListItem
+                onPress={() =>
+                  Linking.openURL(
+                    'https://www.australia.gov.au/about-australia/special-dates-and-events'
+                  )
+                }>
+                <Body>
+                  <Text>Events in Australia</Text>
+                </Body>
+                <Right>
+                  <Icon name="arrow-forward" />
+                </Right>
+              </ListItem>
+            </List>
+          </Content>
+        </Container>
+      </View>
     );
   }
 }
