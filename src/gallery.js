165d5f9268b3275b0efe7a5c4d9a312c0348c7e9
Abhinav Gajurel <abhinav@Abhinavs-MacBook-Pro.local>
2019-09-28 18:13:41 +1000

    add gallery

--- src/gallery.js
+++ src/gallery.js
@@ -1,19 +1,6 @@
 import React, { Component } from 'react';
-import {
-  Container,
-  Content,
-  Card,
-  CardItem,
-  Body,
-  Text,
-  Icon,
-  Button,
-  Right,
-  Left,
-  View,
-  Header
-} from 'native-base';
-import { Image, Modal } from 'react-native';
+import { Content, Card, CardItem, Body, Text, Icon, Right, Left, View, Header } from 'native-base';
+import { Image, Modal, TouchableHighlight } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import ImageViewer from 'react-native-image-zoom-viewer';
 import photo1 from '../assets/photo1.jpg';
@@ -23,26 +10,26 @@ import photo4 from '../assets/photo4.jpg';
 
 const images = [
   {
-    // Simplest usage.
-    // url: "https://avatars2.githubusercontent.com/u/7970947?v=3&s=460",
-    // url:
-    // "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1527660246058&di=6f0f1b19cf05a64317cbc5d2b3713d64&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F0112a85874bd24a801219c7729e77d.jpg",
-    // You can pass props to <Image />.
     props: {
-      // headers: ...
       source: require('../assets/photo1.jpg')
     },
     freeHeight: true
   },
   {
-    // Simplest usage.
-    // url: "https://avatars2.githubusercontent.com/u/7970947?v=3&s=460",
-    // url:
-    // "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1527660246058&di=6f0f1b19cf05a64317cbc5d2b3713d64&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F0112a85874bd24a801219c7729e77d.jpg",
-    // You can pass props to <Image />.
     props: {
-      // headers: ...
       source: require('../assets/photo2.jpg')
+    },
+    freeHeight: true
+  },
+  {
+    props: {
+      source: require('../assets/photo3.jpg')
+    },
+    freeHeight: true
+  },
+  {
+    props: {
+      source: require('../assets/photo4.jpg')
     },
     freeHeight: true
   }
@@ -54,10 +41,14 @@ export default class Gallery extends Component {
     drawerIcon: () => <Icon name="albums" />
   };
 
-  state = {
-    index: 0,
-    modalVisible: true
-  };
+  constructor() {
+    super();
+
+    this.state = {
+      index: 0,
+      modalVisible: false
+    };
+  }
 
   render() {
     return (
@@ -80,39 +71,74 @@ export default class Gallery extends Component {
         <ScrollView>
           <Content padder>
             <Card>
-              <CardItem cardBody>
+              <TouchableHighlight
+                onPress={() => {
+                  console.log('Press event');
+                  this.setState({ index: 0, modalVisible: true });
+                }}>
                 <Image source={photo1} style={{ height: 200, width: null, flex: 1 }} />
-              </CardItem>
+              </TouchableHighlight>
               <CardItem style={{ justifyContent: 'center' }}>
                 <Text>Grace client meeting</Text>
               </CardItem>
             </Card>
             <Card>
-              <CardItem cardBody>
+              <TouchableHighlight
+                onPress={() => {
+                  console.log('Press event');
+                  this.setState({ index: 1, modalVisible: true });
+                }}>
                 <Image source={photo2} style={{ height: 200, width: null, flex: 1 }} />
-              </CardItem>
+              </TouchableHighlight>
+
               <CardItem style={{ justifyContent: 'center' }}>
                 <Text>Chairman</Text>
               </CardItem>
             </Card>
             <Card>
-              <CardItem cardBody>
+              <TouchableHighlight
+                onPress={() => {
+                  console.log('Press event');
+                  this.setState({ index: 2, modalVisible: true });
+                }}>
                 <Image source={photo3} style={{ height: 200, width: null, flex: 1 }} />
-              </CardItem>
+              </TouchableHighlight>
+
               <CardItem style={{ justifyContent: 'center' }}>
                 <Text>Grace Award</Text>
               </CardItem>
             </Card>
             <Card>
-              <CardItem cardBody>
+              <TouchableHighlight
+                onPress={() => {
+                  console.log('Press event');
+                  this.setState({ index: 3, modalVisible: true });
+                }}>
                 <Image source={photo4} style={{ height: 200, width: null, flex: 1 }} />
-              </CardItem>
+              </TouchableHighlight>
+
               <CardItem style={{ justifyContent: 'center' }}>
                 <Text>Grace Discounts</Text>
               </CardItem>
             </Card>
           </Content>
         </ScrollView>
+
+        <View
+          style={{
+            padding: 10
+          }}>
+          <Modal
+            visible={this.state.modalVisible}
+            transparent
+            onRequestClose={() => this.setState({ modalVisible: false })}>
+            <ImageViewer
+              imageUrls={images}
+              index={this.state.index}
+              onMove={data => console.log(data)}
+            />
+          </Modal>
+        </View>
       </View>
     );
   }
