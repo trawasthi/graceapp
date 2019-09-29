220bdf2b75a6b26858b5d3bc5addab4fc790f6d0
Abhinav Gajurel <abhinav.gajurel@gmail.com>
2019-09-29 18:34:23 +1000

    migration add

--- src/migration.js
+++ src/migration.js
@@ -1,5 +1,50 @@
 import React, { Component } from 'react';
-import { Container, Content, Card, CardItem, Body, Text } from 'native-base';
+import {
+  Container,
+  Content,
+  Card,
+  CardItem,
+  Body,
+  Text,
+  List,
+  ListItem,
+  Right,
+  Icon,
+  Accordion
+} from 'native-base';
+import { Linking } from 'react-native';
+
+const migrationDataArray = [
+  {
+    title: 'SKILLED INDEPENDENT PR (189 VISA)',
+    content:
+      'If you are living overseas and wish to enhance your skills and explore yourself to \
+gain global experience then Australia is the country you. Australian government \
+provides you various visa types which allow you to migrate to Australia based on \
+your skills, experience and education etc. Following are the Visa Subclasses that \
+allows you to migrate to Australia and gives you options for Permanent Residency \
+as well.'
+  },
+  {
+    title: 'FAMILY VISA',
+    content:
+      'Family visa by Department of Immigration and Border Protection allows you to bring \
+    your family (Includes Parent, Spouse, De Facto, Child, other family member) to \
+    Australia subject to certain conditions. These visas are sponsored by Australian \
+    Citizens, permanent residents and New Zealand eligible citizens. Grace Group \
+    provides an exceptional customer service and advice for family relating visas. We \
+    bring loved ones together.'
+  },
+  {
+    title: 'EMPLOYER SPONSORED VISAS',
+    content:
+      'Employer Sponsored Visa is the key for skilled people having skills in particular \
+    occupations to work and live in Australia if sponsored by an Australian business. \
+    Also an employer in need of skilled workers can sponsor prospective people to work \
+    in Australia if the occupation is in the SOL, CSOL list. Employer sponsored visas \
+    are categorized into three different subclasses.'
+  }
+];
 
 export default class Migration extends Component {
   static navigationOptions = {
@@ -27,77 +72,21 @@ export default class Migration extends Component {
               </Body>
             </CardItem>
           </Card>
-          <Card>
-            <CardItem>
-              <Text style={{ fontWeight: 'bold' }}>SKILLED INDEPENDENT PR (189 VISA)</Text>
-            </CardItem>
-            <CardItem>
-              <Body>
-                <Text>
-                  If you are living overseas and wish to enhance your skills and explore yourself to
-                  gain global experience then Australia is the country you. Australian government
-                  provides you various visa types which allow you to migrate to Australia based on
-                  your skills, experience and education etc. Following are the Visa Subclasses that
-                  allows you to migrate to Australia and gives you options for Permanent Residency
-                  as well.
-                </Text>
-              </Body>
-            </CardItem>
-          </Card>
-          <Card>
-            <CardItem>
-              <Text style={{ fontWeight: 'bold' }}>FAMILY VISA</Text>
-            </CardItem>
-            <CardItem>
-              <Body>
-                <Text>
-                  Family visa by Department of Immigration and Border Protection allows you to bring
-                  your family (Includes Parent, Spouse, De Facto, Child, other family member) to
-                  Australia subject to certain conditions. These visas are sponsored by Australian
-                  Citizens, permanent residents and New Zealand eligible citizens. Grace Group
-                  provides an exceptional customer service and advice for family relating visas. We
-                  bring loved ones together.
-                </Text>
-              </Body>
-            </CardItem>
-          </Card>
+
+          <Accordion padder dataArray={migrationDataArray} />
 
           <Card>
-            <CardItem>
-              <Text style={{ fontWeight: 'bold' }}>EMPLOYER SPONSORED VISAS</Text>
-            </CardItem>
-            <CardItem>
-              <Body>
-                <Text>
-                  Employer Sponsored Visa is the key for skilled people having skills in particular
-                  occupations to work and live in Australia if sponsored by an Australian business.
-                  Also an employer in need of skilled workers can sponsor prospective people to work
-                  in Australia if the occupation is in the SOL, CSOL list. Employer sponsored visas
-                  are categorized into three different subclasses.
-                </Text>
-              </Body>
-            </CardItem>
-          </Card>
-
-          <Card>
-            <CardItem>
-              <Text style={{ fontWeight: 'bold' }}>SKILLED MIGRATION-GSM VISA</Text>
-            </CardItem>
-            <CardItem>
-              <Body>
-                <Text>
-                  A General Skilled Migration visa is the key to opportunities for people who have
-                  skills and much experience to work in Australia. This visa allows a skilled
-                  migrant and a skilled worker to use their qualifications, work experience, skills
-                  and willingness to meet Australian Immigration Requirements for a Permanent
-                  Australian Visa or Australian Permanent Residency. Grace Group's Migration
-                  department incorporates an experienced team of Migration agents and advisors who
-                  expertise in this Visa. After analyzing the skills and qualifications of the
-                  skilled migrant or worker, we provide an exceptional advice and prepare a very
-                  understood skillful application that provides key to success.
-                </Text>
-              </Body>
-            </CardItem>
+            <List>
+              <ListItem
+                onPress={() => Linking.openURL('http://www.gracegroup.com.au/migration.php')}>
+                <Body>
+                  <Text>More Information</Text>
+                </Body>
+                <Right>
+                  <Icon name="arrow-forward" />
+                </Right>
+              </ListItem>
+            </List>
           </Card>
         </Content>
       </Container>
