981727c7334f72019cd46e2ddeb42b51770b4664
Abhinav Gajurel <abhinav.gajurel@gmail.com>
2019-09-30 17:00:17 +1000

    add fonts

--- App.js
+++ App.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import * as Font from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
 import Navigation from './src/navigation';
 import Firebase, { FirebaseContext } from './src/firebase/firebase';
 
 class App extends React.Component {
+  async componentDidMount() {
+    await Font.loadAsync({
+      Roboto: require('native-base/Fonts/Roboto.ttf'),
+      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+      ...Ionicons.font
+    });
+  }
+
   render() {
     return (
       <FirebaseContext.Provider value={new Firebase()}>
