3b42fe0bc6a5d2557864938a7b5c0182f718caa4
trawasthi <awasthi.tr@gmail.com>
2022-02-18 13:44:51 +1000

    new2

--- src/register.js
+++ src/register.js
@@ -22,7 +22,7 @@ class RegisterScreen extends React.Component {
         {this.state.errorMessage && <Text style={{ color: 'red' }}>{this.state.errorMessage}</Text>}
         <Item style={{ width: 300 }}>
           <Input
-            placeholder="Email address"
+            placeholder="Email"
             autoCapitalize="none"
             onChangeText={email => this.setState({ email })}
             value={this.state.email}
